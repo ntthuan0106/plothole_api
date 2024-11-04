8a05833cb9962e3915607c392085fa4e5571b30f
ntthuan0106 <108946930+ntthuan0106@users.noreply.github.com>
2024-11-05 05:14:49 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,23 +1,27 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using PotholeDetectionApi.Data;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Add services to the container
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Configure the database context for Identity
 builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Configure Identity
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<DataContext>()
+    .AddDefaultTokenProviders();
+
+// Configure JWT Authentication
 builder.Services.AddAuthentication(options =>
 {
     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
@@ -36,23 +40,21 @@ builder.Services.AddAuthentication(options =>
     };
 });
 
-
+// Add authorization
 builder.Services.AddAuthorization();
-
-builder.Services.AddIdentityApiEndpoints<IdentityUser>().AddEntityFrameworkStores<DataContext>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-
+// Configure the HTTP request pipeline
+if (app.Environment.IsDevelopment())
+{
     app.UseSwagger();
     app.UseSwaggerUI();
-
-
-app.MapIdentityApi<IdentityUser>();
+}
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();  // Important: Use Authentication before Authorization
 app.UseAuthorization();
 
 app.MapControllers();
